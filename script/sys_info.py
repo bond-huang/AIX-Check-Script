b21ec77ba83f0e566326d51e53dcea2722eeec77
bond-huang <58139438+bond-huang@users.noreply.github.com>
2020-12-29 10:53:51 +0800

    Update sys_info.py

--- script/sys_info.py
+++ script/sys_info.py
@@ -1,52 +1,67 @@
+#!/usr/bin/python3
+##################################################################################
+############### Collect basic information about the AIX system! ##################
+##################################################################################
 import os
 class GetInfo():
+    # Get Power system Model and Type
     def get_type(self):
         cmd_type = 'uname -M |awk -F, \'{print $2}\''
         mtype = os.popen(cmd_type)
         mtype = mtype.read()
         return mtype.strip()
+    # Get Power system Serial Number
     def get_sn(self):
         cmd_sn = 'prtconf |sed -n \'/Machine Serial Number/p\'|awk \'{print $4}\''
         sn = os.popen(cmd_sn)
         sn = sn.read()
         return sn.strip()
+    # Get Power system Firmware level
     def get_fw(self):
         cmd_fw = 'prtconf | grep \"Platform Firmware level\"|awk \'{print $4}\''
         fw = os.popen(cmd_fw)
         fw = fw.read()
         return fw.strip()
+    # Get AIX system date
     def get_time(self):
         pmdate = os.popen('date +%Y/%m/%d')
         pmdate = pmdate.read()
         return pmdate.strip()
+    # Get AIX system host name
     def get_hostname(self):
         hostname = os.popen('hostname')
         hostname = hostname.read()
         return hostname.strip()
+    # Get AIX system level
     def get_oslevel(self):
         oslevel = os.popen('oslevel -s')
         oslevel = oslevel.read()
         return oslevel.strip()
+    # Get AIX system CPU configuration
     def get_cpu(self):
         cmd_cpu = 'lparstat -i |awk \'/Entitled Capacity[[:blank:]]{2}/{print $4}\''
         cpu = os.popen(cmd_cpu)
         cpu = cpu.read()
         return cpu.strip()
+    # Get AIX system physical memory configuration
     def get_mem(self):
         cmd_mem = 'prtconf -m | awk \'{print $3 $4}\''
         memory = os.popen(cmd_mem)
         memory = memory.read()
         return memory.strip()
+    # Get AIX system page space configuration
     def get_ps(self):
         cmd_ps = 'lsps -a | awk \'{if ($1 == \"hd6\") print $4}\''
         ps = os.popen(cmd_ps)
         ps = ps.read()
         return ps.strip()
+    # Get AIX system IP configuration
     def get_ip(self):
         cmd_ip = 'prtconf |sed -n \'/IP Address:/p\'|awk \'{print $3}\''
         ip = os.popen(cmd_ip)
         ip = ip.read()
         return ip.strip()
+    # Organize the data into a dictionary and add it to the list
     def info_arrange(self):
         item_list = []
         mtype = {'title':'Machine Typy','value':self.get_type()}
